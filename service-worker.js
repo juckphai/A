49bbbd57f1e35a13470015d62cb5890a16cfedc1
juckphai <llsvuslnw@gmail.com>
2025-10-31 20:50:56 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // กำหนดชื่อ Cache
-const staticCacheName = 'account-app-static-v89';
-const dynamicCacheName = 'account-app-dynamic-89';
+const staticCacheName = 'account-app-static-v49';
+const dynamicCacheName = 'account-app-dynamic-v49';
 
 // ไฟล์ที่ต้องการ cache
 const assets = [
